701deb9607246b0a27cd9b443af0c34d2b7fba54
overwolf <vgalvez@t4xi.com>
2022-10-21 08:47:10 +0200

    [FEATURE] Add Feature class

---
+++ src/Feature.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+class Feature
+{
+    const BRANCH_NAME = 'feature_rebase';
+
+    public function getBranchName(): string
+    {
+        return self::BRANCH_NAME;
+    }
+}
