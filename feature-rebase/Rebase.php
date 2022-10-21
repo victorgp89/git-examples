fa6937ea4905d7f1e55ccaa4c42d825f98b3b55f
overwolf <vgalvez@t4xi.com>
2022-10-21 08:48:25 +0200

    [FEATURE] Add properties

--- feature-rebase/Rebase.php
+++ feature-rebase/Rebase.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 class Rebase
 {
     public function __construct(
-        private readonly int $code
+        private readonly int $code,
+        private readonly int $number,
+        private readonly string $name,
     ){
     }
 
     public function code(): int
     {
         return $this->code;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function number(): int
+    {
+        return $this->number;
     }
 
 }
